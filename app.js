5bc7a008dfd3ccf1859cf18cd336203d2c58e878
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-05-04 10:10:06 +0000

    style list with tasks and task

--- app.js
+++ app.js
@@ -77,8 +77,10 @@ class Task extends React.Component {
             <li className="task" id={this.props.id}>
                 <div className="task-info">
                     <p className="task-id">{this.props.id}</p>
-                    <p className="task-date-day">{this.props.date.day}</p>
-                    <p className="task-date-momment">{this.props.date.moment}</p>
+                    <div className="date">
+                        <p className="task-date-day">{this.props.date.day}</p>
+                        <p className="task-date-momment">{this.props.date.moment}</p>
+                    </div>
                 </div>
                 <p className="task-name">{this.props.name}</p>
             </li>
