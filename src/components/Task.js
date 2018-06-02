84934786766f1ad4eece702dc75b25a15a901947
dragosbus <dragosbusuioc94@gmail.com>
2018-06-02 12:00:01 +0200

    changed task list component from a class component to a function component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
 
-export class Task extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    removeTask(e) {
-        console.log(e.target.parentNode.parentNode);
-    }
-
-    render() {
-        return (
-            <li className="task" id={this.props.id}>
-                <div className="task-info">
-                    <p className="task-id">{this.props.id}</p>
-                    <div className="date">
-                        <p className="task-date-day">{this.props.date.day}</p>
-                        <p className="task-date-momment">{this.props.date.moment}</p>
-                    </div>
+export const Task = props =>{
+    return (
+        <li className="task" id={props.id}>
+            <div className="task-info">
+                <p className="task-id">{props.id}</p>
+                <div className="date">
+                    <p className="task-date-day">{props.date.day}</p>
+                    <p className="task-date-momment">{props.date.moment}</p>
                 </div>
-                <div className="task-name">
-                    <p>{this.props.name}</p>
-                    <button onClick={this.removeTask.bind(this)}>Remove</button>
-                </div>
-            </li>
-        );
-    }
-}
+            </div>
+            <div className="task-name">
+                <p>{this.props.name}</p>
+                <button onClick={props.removeTask}>Remove</button>
+            </div>
+        </li>
+    );
+};
