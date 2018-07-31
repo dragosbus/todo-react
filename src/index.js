1bc961cbbf955119bb3828eced63258b6aa85d40
dragosbus <dragosbusuioc94@gmail.com>
2018-07-31 18:58:43 +0200

    added tasks to localstorage

--- src/index.js
+++ src/index.js
@@ -6,8 +6,18 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import gReducer from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
+import {loadState, saveState} from './localstorage';
 
-const store = createStore(gReducer);
+const persistedState = loadState();
+const store = createStore(gReducer, persistedState);
+
+store.subscribe(()=> {
+    saveState({
+        taskReducer: store.getState().taskReducer
+    });
+});
+
+console.log(store.getState())
 
 ReactDOM.render(
     <Provider store={store}>
