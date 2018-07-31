66c5c663588125bea88de4e9c133abd87a7ddb5b
dragosbus <dragosbusuioc94@gmail.com>
2018-07-31 18:48:19 +0200

    refactor actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,38 +1,28 @@
 import * as ActionTypes from '../actionTypes/actionTypes';
 
-export const addTask = (id, name) => {
-    return {
-        type: ActionTypes.ADD_TASK,
-        name,
-        id
-    }
-};
+export const addTask = (id, name) => ({
+    type: ActionTypes.ADD_TASK,
+    name,
+    id
+});
 
-export const removeTask = index => {
-    return {
-        type: ActionTypes.REMOVE_TASK,
-        index
-    }
-};
+export const removeTask = index => ({
+    type: ActionTypes.REMOVE_TASK,
+    index
+});
 
-export const editTask = (index, name) => {
-    return {
-        type: ActionTypes.EDIT_TASK,
-        index,
-        name
-    }
-};
+export const editTask = (index, name) => ({
+    type: ActionTypes.EDIT_TASK,
+    index,
+    name
+});
 
-export const completeTask = index => {
-    return {
-        type: ActionTypes.COMPLETE_TASK,
-        index
-    }
-};
+export const completeTask = index => ({
+    type: ActionTypes.COMPLETE_TASK,
+    index
+});
 
-export const visibilityFilter = filter => {
-    return {
-        type: ActionTypes.VISIBILITY_FILTER,
-        filter
-    }
-};
+export const visibilityFilter = filter => ({
+    type: ActionTypes.VISIBILITY_FILTER,
+    filter
+});
