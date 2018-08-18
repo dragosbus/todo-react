43aa5c876e0a2844d7837848a78c125b3cd7df0d
dragosbus <dragosbusuioc94@gmail.com>
2018-08-18 10:45:19 +0200

    added icon for submit button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {v4} from 'node-uuid';
+import {FaPlusCircle} from 'react-icons/fa';
 
 export class Form extends Component {
     
@@ -12,8 +13,10 @@ export class Form extends Component {
         return (
             <main>
                 <form onSubmit={this.addTask.bind(this)}>
-                    <input type="text" className="form-value" ref={val => this._taskValue = val} />
-                    <input type="submit" className="form-submit" />
+                    <input type="text" className="form-value" ref={val => this._taskValue = val} placeholder="Add a new Task"/>
+                    <button className="form-submit" type="submit">
+                        <FaPlusCircle/>
+                    </button>
                 </form>
             </main>
         );
